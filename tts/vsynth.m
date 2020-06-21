7432a6e0b22861214746fbc6e5c1c4ce0b110624
NithaDuff <chalilnitha007@gmail.com>
2020-06-21 14:46:14 +0530

    process updated to for every delim

--- tts/vsynth.m
+++ tts/vsynth.m
@@ -3,6 +3,15 @@ close all
 clear h
 
 graphics_toolkit qt
+function wav = tts_process(h, str)
+  %str
+  lines = strsplit(str, ".\n")
+  %printf("s = %s", lines{1});
+   for i = 1:length(lines)
+     wav = invoke(h.tts.SV,'Speak',lines{i})
+   %  plot(h.wavplot, wav);
+   end
+endfunction
 
 function update_plot (obj, init = false)
 
@@ -13,8 +22,7 @@ function update_plot (obj, init = false)
   switch (gcbo)
     case {h.synth_pushbutton}
       v = get (h.text_in_edit, "string");
- 
-      invoke(h.tts.SV,'Speak',v); 
+      tts_process(h, v);
        
     case {h.text_in_edit}
       % printf("enter text in");
@@ -40,17 +48,23 @@ h.tts = setup_tts();
 
 h.pitches = [3, 6, 1];
 
+
+%axes ('position', [0.1, 0.3, 0.8, 0.6]);
+%t = linspace (0, 8*pi, 100);  x = t .* cos(t);  y = t .* sin(t);
+%plot (x, y);  axis ([-100, 100, -100, 100]);
+
+p = uipanel ("title", "Stext to Speech Synthesiser", "position", [0 0 1 1]);
  
 ## text entry
-h.plot_title_label = uicontrol ("style", "text",
+h.plot_title_label = uicontrol ("parent", p, "style", "text",
                                 "units", "normalized",
                                 "string", "Input Text",
                                 "horizontalalignment", "left",
                                 "position", [0.05 0.85 0.35 0.08]);
 
-h.text_in_edit = uicontrol ("style", "edit",
+h.text_in_edit = uicontrol ("parent", p, "style", "edit",
                                "units", "normalized",
-                               "string", " Enter text here...(edit)",
+                               "string", " one two three. four five six. seven eigth nine. end",
                                "callback", @update_plot,
                                'backgroundcolor', 'white',
                                'horizontalalignment', 'left',
@@ -59,20 +73,20 @@ h.text_in_edit = uicontrol ("style", "edit",
 
 
 ## Synthesis command 
-h.synth_pushbutton = uicontrol ("style", "pushbutton",
+h.synth_pushbutton = uicontrol ("parent", p, "style", "pushbutton",
                                 "units", "normalized",
                                 "string", "Synthesize",
                                 "callback", @update_plot,
                                 "position", [0.6 0.4 0.35 0.09]);
 
 ## volume label
-h.volume_label = uicontrol ("style", "text",
+h.volume_label = uicontrol ("parent", p, "style", "text",
                            "units", "normalized",
                            "string", "Volume:",
                            "horizontalalignment", "left",
                            "position", [0.05 0.12 0.35 0.08]);
 
-h.volume_slider = uicontrol ("style", "slider",
+h.volume_slider = uicontrol ("parent", p, "style", "slider",
                             "units", "normalized",
                             "string", "slider",
                             "callback", @update_plot,
@@ -80,7 +94,7 @@ h.volume_slider = uicontrol ("style", "slider",
                             "position", [0.05 0.05 0.4 0.06]);
 
 ## Pitch label
-h.pitch_label = uicontrol ("style", "text",
+h.pitch_label = uicontrol ("parent", p, "style", "text",
                                "units", "normalized",
                                "string", "Pitch:",
                                "horizontalalignment", "left",
@@ -88,7 +102,7 @@ h.pitch_label = uicontrol ("style", "text",
                                % [0.05 0.32 0.35 0.08]
 
 
-h.pitch_list = uicontrol ("style", "listbox",
+h.pitch_list = uicontrol ("parent", p, "style", "listbox",
                                 "units", "normalized",
                                 "string", { 
                                            "Normal",
